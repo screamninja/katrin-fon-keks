dfbfa827e2237fa2d2a116bb6895807f0fafe7c3
screamninja <screamninja@gmail.com>
2018-06-08 00:09:32 +0300

    katrin view @ /katrin

---
+++ resources/views/katrin/index.blade.php
@@ -0,0 +1,383 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Катерина Фон Кекс</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body class="homepage">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<div id="header-wrapper">
+					<div id="header">
+
+						<!-- Logo -->
+							<h1><a href="index.html">Катрин Фон Кекс</a></h1>
+
+						<!-- Nav -->
+							<nav id="nav">
+								<ul>
+									<li class="current">
+										<a href="#">Меню</a>
+										<ul>
+											<li>
+												<a href="#">Торты</a>
+												<ul>
+													<li><a href="#">День Рождения</a></li>
+													<li><a href="#">Свадебные</a></li>
+													<li><a href="#">Детские</a></li>
+													<li><a href="#">Личные</a></li>
+													<li><a href="#">Корпоративные</a></li>
+												</ul>
+											</li>
+											<li><a href="#">Капкейки</a></li>
+											<li><a href="#">Пирожные</a></li>
+											<li><a href="#">Вкусняшки</a></li>
+										</ul>
+									</li>
+									<li><a href="left-sidebar.html">Кондитерская школа</a></li>
+									<li><a href="right-sidebar.html">Рецепты</a></li>
+									<li><a href="#">Обо мне</a></li>
+									<li><a href="#">Контакты</a></li>
+								</ul>
+							</nav>
+
+						<!-- Banner -->
+							<section id="banner">
+								<header>
+									<h2>Счастье есть...</h2>
+									<p>позволь себе кусочек</p>
+								</header>
+							</section>
+
+						<!-- Intro -->
+							<section id="intro" class="container">
+								<div class="row">
+									<div class="4u 12u(mobile)">
+										<section class="first">
+											<i class="icon featured fa-cog"></i>
+											<header>
+												<h2>Акции недели!</h2>
+											</header>
+											<p>Nisl amet dolor sit ipsum veroeros sed blandit consequat veroeros et magna tempus.</p>
+										</section>
+									</div>
+									<div class="4u 12u(mobile)">
+										<section class="middle">
+											<i class="icon featured alt fa-flash"></i>
+											<header>
+												<h2>В предверии праздника</h2>
+											</header>
+											<p>Nisl amet dolor sit ipsum veroeros sed blandit consequat veroeros et magna tempus.</p>
+										</section>
+									</div>
+									<div class="4u 12u(mobile)">
+										<section class="last">
+											<i class="icon featured alt2 fa-star"></i>
+											<header>
+												<h2>Лидеры продаж</h2>
+											</header>
+											<p>Nisl amet dolor sit ipsum veroeros sed blandit consequat veroeros et magna tempus.</p>
+										</section>
+									</div>
+								</div>
+								<footer>
+									<ul class="actions">
+										<li><a href="#" class="button big">ЗАКАЗАТЬ</a></li>
+										<li><a href="#" class="button alt big">Доставка и оплата</a></li>
+									</ul>
+								</footer>
+							</section>
+
+					</div>
+				</div>
+
+			<!-- Main -->
+				<div id="main-wrapper">
+					<div class="container">
+						<div class="row">
+							<div class="12u">
+
+								<!-- Portfolio -->
+									<section>
+										<header class="major">
+											<h2>Моё сладкое портфолио</h2>
+										</header>
+										<div class="row">
+											<div class="4u 12u(mobile)">
+												<section class="box">
+													<a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
+													<header>
+														<h3>Ipsum feugiat et dolor</h3>
+													</header>
+													<p>Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit  adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.</p>
+													<footer>
+														<a href="#" class="button alt">Подробнее</a>
+													</footer>
+												</section>
+											</div>
+											<div class="4u 12u(mobile)">
+												<section class="box">
+													<a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
+													<header>
+														<h3>Sed etiam lorem nulla</h3>
+													</header>
+													<p>Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit  adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.</p>
+													<footer>
+														<a href="#" class="button alt">Подробнее</a>
+													</footer>
+												</section>
+											</div>
+											<div class="4u 12u(mobile)">
+												<section class="box">
+													<a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
+													<header>
+														<h3>Consequat et tempus</h3>
+													</header>
+													<p>Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit  adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.</p>
+													<footer>
+														<a href="#" class="button alt">Подробнее</a>
+													</footer>
+												</section>
+											</div>
+										</div>
+										<div class="row">
+											<div class="4u 12u(mobile)">
+												<section class="box">
+													<a href="#" class="image featured"><img src="images/pic05.jpg" alt="" /></a>
+													<header>
+														<h3>Blandit sed adipiscing</h3>
+													</header>
+													<p>Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit  adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.</p>
+													<footer>
+														<a href="#" class="button alt">Подробнее</a>
+													</footer>
+												</section>
+											</div>
+											<div class="4u 12u(mobile)">
+												<section class="box">
+													<a href="#" class="image featured"><img src="images/pic06.jpg" alt="" /></a>
+													<header>
+														<h3>Etiam nisl consequat</h3>
+													</header>
+													<p>Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit  adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.</p>
+													<footer>
+														<a href="#" class="button alt">Подробнее</a>
+													</footer>
+												</section>
+											</div>
+											<div class="4u 12u(mobile)">
+												<section class="box">
+													<a href="#" class="image featured"><img src="images/pic07.jpg" alt="" /></a>
+													<header>
+														<h3>Dolore nisl feugiat</h3>
+													</header>
+													<p>Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit  adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.</p>
+													<footer>
+														<a href="#" class="button alt">Подробнее</a>
+													</footer>
+												</section>
+											</div>
+										</div>
+									</section>
+
+							</div>
+						</div>
+						<div class="row">
+							<div class="12u">
+
+								<!-- Blog -->
+									<section>
+										<header class="major">
+											<h2>Рецепты от Катрин Фон Кекс</h2>
+										</header>
+										<div class="row">
+											<div class="6u 12u(mobile)">
+												<section class="box">
+													<a href="#" class="image featured"><img src="images/pic08.jpg" alt="" /></a>
+													<header>
+														<h3>Ванильные кексы с лимонной начинкой и апельсиновым кремом</h3>
+														<p>Опубликовано 6 минут назад</p>
+													</header>
+													<p>Lorem ipsum dolor sit amet sit veroeros sed et blandit consequat sed veroeros lorem et blandit  adipiscing feugiat phasellus tempus hendrerit, tortor vitae mattis tempor, sapien sem feugiat sapien, id suscipit magna felis nec elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos lorem ipsum dolor sit amet.</p>
+													<footer>
+														<ul class="actions">
+															<li><a href="#" class="button icon fa-file-text">Читать дальше...</a></li>
+															<li><a href="#" class="button alt icon fa-comment">Комментарии (33)</a></li>
+														</ul>
+													</footer>
+												</section>
+											</div>
+											<div class="6u 12u(mobile)">
+												<section class="box">
+													<a href="#" class="image featured"><img src="images/pic09.jpg" alt="" /></a>
+													<header>
+														<h3>Торт "Красный Бархат"</h3>
+														<p>Опубликовано 3 дня назад</p>
+													</header>
+													<p>Lorem ipsum dolor sit amet sit veroeros sed et blandit consequat sed veroeros lorem et blandit  adipiscing feugiat phasellus tempus hendrerit, tortor vitae mattis tempor, sapien sem feugiat sapien, id suscipit magna felis nec elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos lorem ipsum dolor sit amet.</p>
+													<footer>
+														<ul class="actions">
+															<li><a href="#" class="button icon fa-file-text">Читать дальше...</a></li>
+															<li><a href="#" class="button alt icon fa-comment">Комментарии (42)</a></li>
+														</ul>
+													</footer>
+												</section>
+											</div>
+										</div>
+									</section>
+
+							</div>
+						</div>
+					</div>
+				</div>
+
+			<!-- Footer -->
+				<div id="footer-wrapper">
+					<section id="footer" class="container">
+						<div class="row">
+							<div class="8u 12u(mobile)">
+								<section>
+									<header>
+										<h2>Ближайшие мастер-классы</h2>
+									</header>
+									<ul class="dates">
+										<li>
+											<span class="date">Сен <strong>27</strong></span>
+											<h3><a href="#">Lorem dolor sit amet veroeros</a></h3>
+											<p>Ipsum dolor sit amet veroeros consequat blandit ipsum phasellus lorem consequat etiam.</p>
+										</li>
+										<li>
+											<span class="date">Ноя <strong>23</strong></span>
+											<h3><a href="#">Ipsum sed blandit nisl consequat</a></h3>
+											<p>Blandit phasellus lorem ipsum dolor tempor sapien tortor hendrerit adipiscing feugiat lorem.</p>
+										</li>
+										<li>
+											<span class="date">Окт <strong>12</strong></span>
+											<h3><a href="#">Magna tempus lorem feugiat</a></h3>
+											<p>Dolore consequat sed phasellus lorem sed etiam nullam dolor etiam sed amet sit consequat.</p>
+										</li>
+										<li>
+											<span class="date">Окт <strong>12</strong></span>
+											<h3><a href="#">Dolore tempus ipsum feugiat nulla</a></h3>
+											<p>Feugiat lorem dolor sed nullam tempus lorem ipsum dolor sit amet nullam consequat.</p>
+										</li>
+										<li>
+											<span class="date">Дек <strong>10</strong></span>
+											<h3><a href="#">Blandit tempus aliquam?</a></h3>
+											<p>Feugiat sed tempus blandit tempus adipiscing nisl lorem ipsum dolor sit amet dolore.</p>
+										</li>
+									</ul>
+								</section>
+							</div>
+							<div class="4u 12u(mobile)">
+								<section>
+									<header>
+										<h2>Хотите сотрудничать?</h2>
+									</header>
+									<a href="#" class="image featured"><img src="images/pic10.jpg" alt="" /></a>
+									<p>
+										This is <strong>Dopetrope</strong> a free, fully responsive HTML5 site template by
+										<a href="http://twitter.com/ajlkn">AJ</a> for <a href="http://html5up.net/">HTML5 UP</a> It's released for free under
+										the <a href="http://html5up.net/license/">Creative Commons Attribution</a> license so feel free to use it for any personal or commercial project &ndash; just don't forget to credit us!
+									</p>
+									<footer>
+										<a href="#" class="button">Узнать подробнее</a>
+									</footer>
+								</section>
+							</div>
+						</div>
+						<div class="row">
+							<div class="4u 12u(mobile)">
+								<section>
+									<header>
+										<h2>Информация</h2>
+									</header>
+									<ul class="divided">
+										<li><a href="#">Вопросы/Ответы</a></li>
+										<li><a href="#">Отзывы</a></li>
+										<li><a href="#">Правовая информация</a></li>
+										<li><a href="#">Обратная связ</a></li>
+									</ul>
+								</section>
+							</div>
+							<div class="4u 12u(mobile)">
+								<section>
+									<header>
+										<h2>Помощь</h2>
+									</header>
+									<ul class="divided">
+										<li><a href="#">Как оформить заказ</a></li>
+										<li><a href="#">Способы оплаты</a></li>
+										<li><a href="#">Доставка</a></li>
+										<li><a href="#">Как записаться на мастер-класс</a></li>
+										<li><a href="#">Информация для Юр. лиц</a></li>
+										<li><a href="#">Наши гарантии</a></li>
+									</ul>
+								</section>
+							</div>
+							<div class="4u 12u(mobile)">
+								<section>
+									<header>
+										<h2>Следите за нашими новостями</h2>
+									</header>
+									<ul class="social">
+										<li><a class="icon fa-facebook" href="#"><span class="label">Facebook</span></a></li>
+										<li><a class="icon fa-twitter" href="#"><span class="label">Twitter</span></a></li>
+										<li><a class="icon fa-dribbble" href="#"><span class="label">Dribbble</span></a></li>
+										<li><a class="icon fa-linkedin" href="#"><span class="label">LinkedIn</span></a></li>
+										<li><a class="icon fa-google-plus" href="#"><span class="label">Google+</span></a></li>
+									</ul>
+									<ul class="contact">
+										<li>
+											<h3>Адресс</h3>
+											<p>
+												Untitled Incorporated<br />
+												1234 Somewhere Road Suite<br />
+												Nashville, TN 00000-0000
+											</p>
+										</li>
+										<li>
+											<h3>E-mail</h3>
+											<p><a href="#">someone@untitled.tld</a></p>
+										</li>
+										<li>
+											<h3>Телефон</h3>
+											<p>(800) 000-0000</p>
+										</li>
+									</ul>
+								</section>
+							</div>
+						</div>
+						<div class="row">
+							<div class="12u">
+
+								<!-- Copyright -->
+									<div id="copyright">
+										<ul class="links">
+											<li>&copy; Пекарня "Катрин Фон Кекс". Все права защищены.</li><li>Design: <a href="#">NoName Studio</a></li>
+										</ul>
+									</div>
+
+							</div>
+						</div>
+					</section>
+				</div>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/skel-viewport.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
